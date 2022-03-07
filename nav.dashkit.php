b9a12ad865a06fc1a4f6d18890c122eabc84e533
symbiondk <kbs@symbion.dk>
2022-03-07 10:03:13 +0100

    Added JS controlling active nav item

--- nav.dashkit.php
+++ nav.dashkit.php
@@ -16,7 +16,7 @@ function renderNavItem($nav_item) {
         $NavItemHTML .= 
         '<li class="nav-item">
           <a class="nav-link" href="#item_'.$nav_item['id'].'" role="button" data-bs-toggle="collapse" aria-expanded="false" aria-controls="item_'.$nav_item['id'].'">';
-          $NavItemHTML .= (!empty($nav_item['icon'])) ? '<i class="me-1 '.$nav_item['icon'].'"></i>' : '';
+          $NavItemHTML .= (!empty($nav_item['icon'])) ? '<i class="'.$nav_item['icon'].'"></i>' : '';
           $NavItemHTML .= $nav_item['label'].'</a>
 
           <div class="collapse " id="item_'.$nav_item['id'].'">
@@ -36,7 +36,7 @@ function renderNavItem($nav_item) {
         $NavItemHTML = 
         '<li class="nav-item">
           <a class="nav-link" id="item_'.$nav_item['id'].'" href="'.$nav_item['url'].'">';
-        $NavItemHTML .= (!empty($nav_item['icon'])) ? '<i class="me-1 '.$nav_item['icon'].'"></i>' : '';
+        $NavItemHTML .= (!empty($nav_item['icon'])) ? '<i class="'.$nav_item['icon'].'"></i>' : '';
         $NavItemHTML .= $nav_item['label'].'</a>
         </li>';
     }
@@ -49,3 +49,25 @@ $nav = getNav(); // Load nav data from JSON
  foreach ($nav as $key => $nav_item) { 
   echo renderNavItem($nav_item); 
 } ?> 
+<script>
+
+
+    $(function($) {
+          var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+
+          var lastdir = window.location.href.split('?')[0];
+
+          var pgurl_abs = window.location.href;
+
+            $(".navbar-nav .nav-item .nav-link").each(function(){
+
+                var navdir = $(this).attr("href");
+              
+                if(navdir == pgurl || navdir == pgurl_abs || navdir == lastdir || navdir == '' ) {
+                  $(this).addClass("active");
+                  $(this).closest(".collapse").addClass("show");
+                }
+
+          })
+      });
+</script>
